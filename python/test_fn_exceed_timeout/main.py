2f343e37cd40e9eb072f820ac793e478228829e9
Richard Townsend <richard@sentimentron.co.uk>
2018-03-10 19:40:57 +0000

    Move python bindings to a seperate repository

--- python/test_fn_exceed_timeout/main.py
+++
@@ -1,6 +0,0 @@
-import time
-
-if __name__ == "__main__":
-    time.sleep(5)
-    print("Hello World!")
-
--- python/test_fn_stdin/main.py
+++
@@ -1,6 +0,0 @@
-import fileinput
-
-if __name__ == "__main__":
-    for name in fileinput.input():
-        print("Hello {}".format(name))
-
